e2f51f7b2e6827d50fdec1d035da05094e5f90cc
15219900+Ninjagod1251@users.noreply.github.com <15219900+Ninjagod1251@users.noreply.github.com>
2022-03-24 15:12:33 -0700

    feat: add test

--- cookiecutter-erc721-template/{{cookiecutter.directory_name}}/tests/test_token.py
+++ cookiecutter-erc721-template/{{cookiecutter.directory_name}}/tests/test_token.py
@@ -1,2 +1,10 @@
-def test_token(token):
-    assert token
+
+def test_token(owner, receiver, token):
+    #transaction guide https://docs.apeworx.io/ape/stable/userguides/transactions.html
+    # expect to uses gas
+    token.mint(receiver, 5 ,sender=owner)
+    # contract calls are free so you do not need sender=owner
+
+    acutal = token.balanceOf(receiver)
+    expect = 5
+    assert acutal == expect
