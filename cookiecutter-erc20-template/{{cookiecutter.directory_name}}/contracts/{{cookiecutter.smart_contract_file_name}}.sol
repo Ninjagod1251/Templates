734df160d48691da0387caef6731b300363ce684
15219900+Ninjagod1251@users.noreply.github.com <15219900+Ninjagod1251@users.noreply.github.com>
2022-04-12 09:27:44 -0700

    feat: better erc20 template

--- cookiecutter-erc20-template/{{cookiecutter.directory_name}}/contracts/{{cookiecutter.smart_contract_file_name}}.sol
+++ cookiecutter-erc20-template/{{cookiecutter.directory_name}}/contracts/{{cookiecutter.smart_contract_file_name}}.sol
@@ -23,11 +23,194 @@ contract {{cookiecutter.smart_contract_file_name}} is ERC20 , Ownable
 {%- if cookiecutter.votes == 'y' %}, ERC20Votes  {%- endif %}
 {%- if cookiecutter.flashminting == 'y' %}, ERC20FlashMint  {%- endif %}
 {%- if cookiecutter.snapshot == 'y' %}, ERC20Snapshot  {%- endif %} {
-    constructor() ERC20("{{cookiecutter.token_name}}", "{{cookiecutter.token_symbol}}") {%- if cookiecutter.permit == 'y' or (cookiecutter.votes == 'y') %} ERC20Permit("{{cookiecutter.token_name}}") {%- endif %}{
-{%- if cookiecutter.premint == 'y' %} 
-        _mint(msg.sender, {{cookiecutter.premint_amount}} * 10 ** decimals());
-{%- endif %}
+    
+    mapping(address => uint256) private _balances;
+
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+
+    string private _name;
+    string private _symbol;
+    
+    constructor(string memory name_, string memory symbol_) ERC20("{{cookiecutter.token_name}}", "{{cookiecutter.token_symbol}}") 
+    {%- if cookiecutter.permit == 'y' or (cookiecutter.votes == 'y') %} 
+    ERC20Permit("{{cookiecutter.token_name}}") 
+    {%- endif %}{
+        _name = name_;
+        _symbol = symbol_;
     }
+    
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() public view virtual override returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
+    function symbol() public view virtual override returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei. This is the value {ERC20} uses, unless this function is
+     * overridden;
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() public view virtual override returns (uint8) {
+        return 18;
+    }
+
+    /**
+     * @dev See {IERC20-totalSupply}.
+     */
+    function totalSupply() public view virtual override returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+     * @dev See {IERC20-balanceOf}.
+     */
+    function balanceOf(address account) public view virtual override returns (uint256) {
+        return _balances[account];
+    }
+
+    /**
+     * @dev See {IERC20-transfer}.
+     *
+     * Requirements:
+     *
+     * - `to` cannot be the zero address.
+     * - the caller must have a balance of at least `amount`.
+     */
+    function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        address owner = _msgSender();
+        _transfer(owner, to, amount);
+        return true;
+    }
+
+    /**
+     * @dev See {IERC20-allowance}.
+     */
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+        /**
+     * @dev See {IERC20-approve}.
+     *
+     * NOTE: If `amount` is the maximum `uint256`, the allowance is not updated on
+     * `transferFrom`. This is semantically equivalent to an infinite approval.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     */
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        address owner = _msgSender();
+        _approve(owner, spender, amount);
+        return true;
+    }
+
+    /**
+     * @dev See {IERC20-transferFrom}.
+     *
+     * Emits an {Approval} event indicating the updated allowance. This is not
+     * required by the EIP. See the note at the beginning of {ERC20}.
+     *
+     * NOTE: Does not update the allowance if the current allowance
+     * is the maximum `uint256`.
+     *
+     * Requirements:
+     *
+     * - `from` and `to` cannot be the zero address.
+     * - `from` must have a balance of at least `amount`.
+     * - the caller must have allowance for ``from``'s tokens of at least
+     * `amount`.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        address spender = _msgSender();
+
+{%- if cookiecutter.spend_Allowance == 'y' %} 
+        _spendAllowance(from, spender, amount);
+{%- endif %}
+        _transfer(from, to, amount);
+        return true;
+    }
+
+    /**
+     * @dev Moves `amount` of tokens from `sender` to `recipient`.
+     *
+     * This internal function is equivalent to {transfer}, and can be used to
+     * e.g. implement automatic token fees, slashing mechanisms, etc.
+     *
+     * Emits a {Transfer} event.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `from` must have a balance of at least `amount`.
+     */
+function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override{
+        require(from != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0), "ERC20: transfer to the zero address");
+
+{%- if cookiecutter.before_Token_Transfer == 'y' %} 
+        _beforeTokenTransfer(from, to, amount);
+{%- endif %}
+
+    uint256 fromBalance = _balances[from];
+        require(fromBalance >= amount, "ERC20: transfer amount exceeds balance");
+        unchecked {
+            _balances[from] = fromBalance - amount;
+        }
+        _balances[to] += amount;
+
+        emit Transfer(from, to, amount);
+
+        _afterTokenTransfer(from, to, amount);
+    }
+
+{%- if cookiecutter.before_Token_Transfer == 'y' %} 
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        {%if cookiecutter.pausable == 'y' %}whenNotPaused{%- endif %}
+        {%if cookiecutter.snapshot == 'y' %}override(ERC20, ERC20Snapshot){%- else %}override{%- endif %}
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+{%- endif %}
+
+{%- if cookiecutter.after_Token_Transfer == 'y' %} 
+    // The following functions are overrides required by Solidity.
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        {%if cookiecutter.votes == 'y' %}override(ERC20, ERC20Votes){%- else %}override(ERC20){%- endif %}
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+{%- endif %}
 
 {%- if cookiecutter.snapshot == 'y' %} 
 function snapshot() public onlyOwner {
@@ -51,48 +234,21 @@ function snapshot() public onlyOwner {
         function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
-{%- endif %}
-
-{%- if (cookiecutter.pausable == 'y') or (cookiecutter.snapshot == 'y') %} 
-
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        {%if cookiecutter.pausable == 'y' %}whenNotPaused
-        {%- endif %}
-        {%if cookiecutter.snapshot == 'y' %}override(ERC20, ERC20Snapshot)
-        {%- else %}
-        override
-        {%- endif %}
-    {
-        super._beforeTokenTransfer(from, to, amount);
-    }
-
-{%- endif %}
-
-{%- if cookiecutter.votes == 'y' %} 
-
-    // The following functions are overrides required by Solidity.
-
-    function _afterTokenTransfer(address from, address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
-        super._afterTokenTransfer(from, to, amount);
-    }
 
     function _mint(address to, uint256 amount)
         internal
-        override(ERC20, ERC20Votes)
+        {%if cookiecutter.votes == 'y' %}override(ERC20, ERC20Votes){%- else %}override(ERC20){%- endif %}
     {
         super._mint(to, amount);
     }
+{%- endif %}
 
+{%- if cookiecutter.burnable == 'y' %} 
     function _burn(address account, uint256 amount)
         internal
-        override(ERC20, ERC20Votes)
+        {%if cookiecutter.votes == 'y' %}override(ERC20, ERC20Votes){%- else %}override(ERC20){%- endif %}
     {
         super._burn(account, amount);
     }
-
 {%- endif %}
 }
